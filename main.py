5ab65ac486625e020a17a0c0c8f86f2cb4908021
Yonathan Assefa <yab95assefa@gmail.com>
2023-06-05 19:31:02 +0300

    updated the ship movement from the user input
    Now, the ship moves inside the canvas only

--- main.py
+++ main.py
@@ -22,6 +22,16 @@ class Spaceship(arcade.Sprite):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
+        # Keep the spaceship within the boundaries
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
 class GameWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
