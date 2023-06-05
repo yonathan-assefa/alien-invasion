35e3b4e2a8883ea8a9289dc19d4d1e0f943dc829
Yonathan Assefa <yab95assefa@gmail.com>
2023-06-05 19:28:19 +0300

    some modifications

--- main.py
+++ main.py
@@ -5,24 +5,61 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Alien Invasion"
 
+# Set up constants for the spaceship
+SPACESHIP_SPEED = 5
+SPACESHIP_IMAGE = "img/spaceship.png"
+
+class Spaceship(arcade.Sprite):
+    def __init__(self):
+        super().__init__(SPACESHIP_IMAGE, scale=0.15)
+        self.center_x = SCREEN_WIDTH // 2
+        self.center_y = 50
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+        # Update the spaceship's position based on user input or game logic
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
 class GameWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK)
+        self.spaceship = None
 
     def setup(self):
         # Set up your game here, such as initializing variables and loading resources
-        pass
+        self.spaceship = Spaceship()
 
     def on_draw(self):
         # Render the screen here
         arcade.start_render()
 
-        # Add your drawing code here, such as drawing sprites and UI elements
+        # Draw the spaceship
+        self.spaceship.draw()
 
     def update(self, delta_time):
         # Update the game logic here
-        pass
+        self.spaceship.update()
+
+    def on_key_press(self, key, modifiers):
+        # Handle key press events
+        if key == arcade.key.LEFT:
+            self.spaceship.change_x = -SPACESHIP_SPEED
+        elif key == arcade.key.RIGHT:
+            self.spaceship.change_x = SPACESHIP_SPEED
+        elif key == arcade.key.UP:
+            self.spaceship.change_y = SPACESHIP_SPEED
+        elif key == arcade.key.DOWN:
+            self.spaceship.change_y = -SPACESHIP_SPEED
+
+    def on_key_release(self, key, modifiers):
+        # Handle key release events
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.spaceship.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.spaceship.change_y = 0
 
 def main():
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
